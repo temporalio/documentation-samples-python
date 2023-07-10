48d593501d5d443f04b61667b99a0b2043fb2c05
rachfop <prachford@icloud.com>
2023-07-10 12:39:42 -0700

    update list workflow lines

--- your_visibility/starter_dacx.py
+++ your_visibility/starter_dacx.py
@@ -49,7 +49,7 @@ id: how-to-list-workflow-executions-using-the-client-in-python
 title: How to list Workflows in Python
 label: List Workflows
 description: To list Workflows from the Client, use the [list_workflows()](https://python.temporal.io/temporalio.client.Client.html#list_workflows) method on the Client handle and pass a [List Filter](/concepts/what-is-a-list-filter) as an argument to filter the listed Workflows.
-lines: 11-13, 41-42
+lines: 11-13, 40-41
 @dacx """
 
 
