dba894d267924a48f9cb0fc22530820ac3059384
rachfop <prachford@icloud.com>
2023-09-27 13:39:01 -0700

    Remove bold

--- version_your_workflows/workflow_4_patch_complete_dacx.py
+++ version_your_workflows/workflow_4_patch_complete_dacx.py
@@ -10,9 +10,9 @@ Now, you want to update your code to run `post_patch_activity` instead. This rep
 dacx"""
 
 """dacx
-***Problem: You cannot deploy `post_patch_activity` directly until you're certain there are no more running Workflows created using the `pre_patch_activity` code.**
+**Problem: You cannot deploy `post_patch_activity` directly until you're certain there are no more running Workflows created using the `pre_patch_activity` code.**
 
-Instead, you'll need to deploy **`post_patched_activity`** and use the `[patched](https://python.temporal.io/temporalio.workflow.html#patched)` function to determine which version of the code to execute.
+Instead, you'll need to deploy `post_patched_activity` and use the [patched](https://python.temporal.io/temporalio.workflow.html#patched) function to determine which version of the code to execute.
 
 Implementing patching involves three steps:
 
