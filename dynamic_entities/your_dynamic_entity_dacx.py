60c4767e9a567a7276955ec69ff945202b042742
rachfop <prachford@icloud.com>
2023-10-06 14:08:49 -0700

    remove nums

--- dynamic_entities/your_dynamic_entity_dacx.py
+++ dynamic_entities/your_dynamic_entity_dacx.py
@@ -9,14 +9,14 @@ from temporalio.common import RawValue
 from temporalio.worker import Worker
 
 """dacx
-1) A Workflow can be made dynamic by adding dynamic=True to the `@workflow.defn` decorator.
+A Workflow can be made dynamic by adding dynamic=True to the `@workflow.defn` decorator.
 The Workflow's run method must then accept a single argument of type `Sequence[temporalio.common.RawValue]`.
 At runtime, the Workflow will be invoked if no Workflow with the given name is registered.
 The input arguments are passed to the run method as the single `Sequence[RawValue]` argument.
 dacx"""
 
 """dacx
-2) Similar to dynamic Workflows, an Activity can be made dynamic by adding `dynamic=True` to the `@activity.defn` decorator.
+Similar to dynamic Workflows, an Activity can be made dynamic by adding `dynamic=True` to the `@activity.defn` decorator.
 The Activity function must then accept a single argument of type `Sequence[temporalio.common.RawValue]`.
 
 At runtime, the Activity will be invoked if no Activity with the given name is registered.
@@ -24,13 +24,13 @@ The input arguments are passed to the Activity function as the single `Sequence[
 dacx"""
 
 """dacx
-3) A Signal can be made dynamic by adding dynamic=True to the `@workflow.signal` decorator.
+A Signal can be made dynamic by adding dynamic=True to the `@workflow.signal` decorator.
 The Signal method must accept two arguments - a str name and a `Sequence[temporalio.common.RawValue]` args.
 At runtime, the Signal will be invoked if no Signal with the given name exists on the Workflow.
 dacx"""
 
 """dacx
-4) Similar to dynamic Signals, a Query can be made dynamic by adding `dynamic=True` to the `@workflow.query` decorator.
+Similar to dynamic Signals, a Query can be made dynamic by adding `dynamic=True` to the `@workflow.query` decorator.
 The Query method must accept two arguments - a `str` name and a `Sequence[temporalio.common.RawValue]` args.
 At runtime, the Query will be invoked if no Query with the given name is defined on the Workflow.
 dacx"""
