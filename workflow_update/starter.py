c8add7166374f6437ee591105ff12242ea990ce0
rachfop <prachford@icloud.com>
2023-12-13 09:53:01 -0800

    Adds Workflow Update

---
+++ workflow_update/starter.py
@@ -0,0 +1,45 @@
+import asyncio
+from temporalio.client import Client
+from update_workflow import HelloWorldWorkflow
+
+"""dacx
+To send a Workflow Update from a Temporal Client, set the [execute_update](https://python.temporal.io/temporalio.client.WorkflowHandle.html#execute_update) method from the [WorkflowHandle](https://python.temporal.io/temporalio.client.WorkflowHandle.html) class.
+dacx"""
+
+
+async def main():
+    client = await Client.connect("localhost:7233")
+
+    # Start the workflow
+    handle = await client.start_workflow(
+        HelloWorldWorkflow.run,
+        id="hello-world-workflow",
+        task_queue="update-task-queue",
+    )
+
+    # Perform the update
+    update_result = await handle.execute_update(
+        HelloWorldWorkflow.update_workflow_status
+    )
+    print(f"Update Result: {update_result}")
+
+    # Get the workflow result
+    result = await handle.result()
+    print(f"Workflow Result: {result}")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+""" @dacx
+id: how-to-send-an-update-from-a-client-in-python
+title: How to send an Update from a Temporal Client in Python
+label: Send Update from Client
+description: Use the execute_update method from the WorkflowHandle class to send an Update to a Workflow Execution.
+tags:
+ - python sdk
+ - code sample
+ - workflow
+ - update
+lines: 5-7, 21-24
+@dacx """
---
+++ workflow_update/update_workflow.py
@@ -0,0 +1,48 @@
+from temporalio import workflow
+
+"""dacx
+Workflow Updates handlers are methods in your Workflow Definition designed to handle updates.
+These updates can be triggered during the lifecycle of a Workflow Execution.
+
+**Define an Update Handler**
+
+To define an update handler, use the [@workflow.update](https://python.temporal.io/temporalio.workflow.html#update) decorator on a method within your Workflow. This decorator can be applied to both asynchronous and synchronous methods.
+
+**Update Handler structure**
+
+- **Decorator Usage:** Apply `@workflow.update` to the method intended to handle updates.
+- **Overriding:** If a method with this decorator is overridden, the overriding method should also be decorated with `@workflow.update`.
+- **Validator Method:** Optionally, you can define a validator method for the update handler. This validator is specified using `@update_handler_method_name.validator` and is invoked before the update handler.
+- **Method Parameters:** Update handlers should only use positional parameters. For non-dynamic methods, it's recommended to use a single parameter that is an object or data class, which allows for future expansion of fields.
+- **Return Values:** The update handler can return a serializable value. This value is sent back to the caller of the update.
+dacx"""
+
+
+@workflow.defn
+class HelloWorldWorkflow:
+    is_complete = False
+
+    @workflow.run
+    async def run(self) -> str:
+        await workflow.wait_condition(lambda: self.is_complete)
+        return "Hello, World!"
+
+    # Send an update request to the workflow and wait for it to complete.
+    @workflow.update
+    async def update_workflow_status(self) -> str:
+        self.is_complete = True
+        return "Workflow status updated"
+
+
+""" @dacx
+id: how-to-define-an-update-in-python
+title: How to define an Update in Python
+label: Define Update
+description: Define an Update by giving it a name to identify it.
+tags:
+ - python sdk
+ - code sample
+ - workflow
+ - update
+lines: 3-18, 31-34
+@dacx """
